6d3209f9cb0b3fe3f37a1d7ef91be890ef8058a4
i.voloshchenko <i.voloshchenko@creonit.ru>
2022-12-12 04:45:13 +0300

    feat: file upload endpoint

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\FileUploadType;
+use App\Helper\FileHelper;
+use App\Service\Product\ProductService;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProductController extends AbstractController
+{
+    #[Route('/productImport', name: 'productImport')]
+    public function index(Request $request, FileHelper $fileHelper, ProductService $productService): Response
+    {
+        $form = $this->createForm(FileUploadType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $file = $form['upload_file']->getData();
+
+            if ($file && $fileName = $fileHelper->upload($file)) {
+                $productService->importFromXml($fileName);
+            }
+        }
+
+        return $this->render('product/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
