ce27bb0548872c4a4cc4673adba9b0b554b0fd45
i.voloschenko <i.voloschenko@creonit.ru>
2022-12-13 16:28:06 +0300

    test: corrected parser test

--- tests/Service/Parser/ProductParserTest.php
+++ tests/Service/Parser/ProductParserTest.php
@@ -30,15 +30,21 @@ class ProductParserTest extends WebTestCase
         /** @var ProductCategoryRepository $productCategoryRepository */
         $productCategoryRepository = $doctrine->getRepository(ProductCategory::class);
 
-        $product = $productRepository->findOneByName('in suscipit');
-        $productCategory = $productCategoryRepository->findOneByTitle('et');
+        $products = $productRepository->findAll();
+        $productCategories = $productCategoryRepository->findAll();
 
-        $this->assertEquals('in suscipit', $product->getName());
-        $this->assertEquals('By this time she found herself at last it unfolded its arms', $product->getDescription());
-        $this->assertEquals('30 g', $product->getWeight());
-        $this->assertEquals('et', $product->getCategories()->getValues()[0]->getTitle());
+        $this->assertEquals('in suscipit', $products[0]->getName());
+        $this->assertEquals('By this time she found herself at last it unfolded its arms', $products[0]->getDescription());
+        $this->assertEquals('30 g', $products[0]->getWeight());
+        $this->assertEquals('et', $products[0]->getCategories()->getValues()[0]->getTitle());
+        $this->assertEquals('in suscipit2', $products[1]->getName());
+        $this->assertEquals('By2 this time she found herself at last it unfolded its arms', $products[1]->getDescription());
+        $this->assertEquals('302 g', $products[1]->getWeight());
+        $this->assertEquals('et2', $products[1]->getCategories()->getValues()[0]->getTitle());
 
-        $this->assertEquals('et', $productCategory->getTitle());
-        $this->assertEquals('in suscipit', $productCategory->getProducts()->getValues()[0]->getName());
+        $this->assertEquals('et', $productCategories[0]->getTitle());
+        $this->assertEquals('in suscipit', $productCategories[0]->getProducts()->getValues()[0]->getName());
+        $this->assertEquals('et2', $productCategories[1]->getTitle());
+        $this->assertEquals('in suscipit2', $productCategories[1]->getProducts()->getValues()[0]->getName());
     }
 }
